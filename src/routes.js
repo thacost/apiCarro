4e72a237699b790c7d6c14574e404c67a4b4db86
Thales Costa <ttcosta03@gmail.com>
2023-08-14 23:34:34 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const CarroController = require('./controllers/CarroController');
+
+//ROTAS//
+router.get('/carros', CarroController.buscarTodos);
+router.get('/carro/:codigo', CarroController.buscarUm);
+router.post('/carro', CarroController.inserir);
+router.put('/carro/:codigo', CarroController.alterar);
+router.delete('/carro/:codigo', CarroController.excluir);
+
+
+module.exports = router;
